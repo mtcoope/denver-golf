1a00c89e140a0e2c230b36f82be3265d6bf595d7
Mike <mtc3108@gmail.com>
2022-03-12 18:38:44 -0500

    adding client

--- helpers.js
+++
@@ -1,9 +0,0 @@
-exports.getBestTeeTime = (settings, searchResult) => {
-    let orderedResult = [];
-    settings.courses.forEach(x => {
-        const courseResult = searchResult.filter(y => y.r07 === x).sort();
-        courseResult.forEach(y => orderedResult.push(y));
-        
-    });
-    return orderedResult;
-}
